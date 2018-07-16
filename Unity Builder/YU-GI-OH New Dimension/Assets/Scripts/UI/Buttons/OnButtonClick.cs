d2ec4c0beeb38339284dff5d7fda46ce2a2aa429
blizzard190 <22256@ma-web.nl>
2018-07-16 18:22:06 +0200

    Buttons
    Button scripts + a test Character script

---
+++ Unity Builder/YU-GI-OH New Dimension/Assets/Scripts/UI/Buttons/OnButtonClick.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class OnButtonClick : MonoBehaviour {
+
+    GameObject[] Objects;
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void SceneSwitch(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void MenuSelector(string menu)
+    {
+        Objects = GameObject.FindGameObjectsWithTag(menu);
+        
+        if(Objects[0].activeSelf == true)
+        {
+            HideElement();
+        }
+        else if(Objects[0].activeSelf == false)
+        {
+            ShowElement();
+        }
+    }
+
+    private void ShowElement()
+    {
+        foreach (GameObject g in Objects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    public void HideElement()
+    {
+        foreach (GameObject g in Objects)
+        {
+            g.SetActive(false);
+        }
+    }
+}
